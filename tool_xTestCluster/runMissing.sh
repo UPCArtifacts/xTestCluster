2f5a0f3e7a19c997976c9e49713466031d98c966
martinezmatias <matias.sebastian.martinez@gmail.com>
2023-02-01 18:48:48 +0100

    up

---
+++ tool_xTestCluster/runMissing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+for i in {301..310}
+do
+ echo "$i"
+ ./runGenAndExec.sh Math-59 $i;
+ ./runGenAndExec.sh Lang-24 $i;
+ ./runGenAndExec.sh Closure-31 $i;
+ ./runGenAndExec.sh Math-71 $i;
+ ./runGenAndExec.sh Chart-12 $i;
+ ./runGenAndExec.sh Closure-46 $i;
+ ./runGenAndExec.sh Chart-7 $i;
+ ./runGenAndExec.sh Math-70 $i;
+ ./runGenAndExec.sh Lang-10 $i;
+ ./runGenAndExec.sh Lang-24 $i;
+ ./runGenAndExec.sh Math-57 $i;
+ ./runGenAndExec.sh Lang-39 $i;
+ ./runGenAndExec.sh Lang-45 $i;
+ ./runGenAndExec.sh Lang-55 $i;
+ ./runGenAndExec.sh Math-4 $i;
+ ./runGenAndExec.sh Lang-43 $i;
+ ./runGenAndExec.sh Closure-14 $i;
+ ./runGenAndExec.sh Closure-18 $i;
+ ./runGenAndExec.sh Lang-6 $i;
+ ./runGenAndExec.sh Math-33 $i;
+ ./runGenAndExec.sh Math-58 $i;
+ ./runGenAndExec.sh Closure-115 $i;
+ ./runGenAndExec.sh Closure-62 $i;
+ ./runGenAndExec.sh Closure-126 $i;
+ ./runGenAndExec.sh Closure-38 $i;
+done
