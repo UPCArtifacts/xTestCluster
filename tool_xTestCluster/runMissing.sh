929fc62518843a7fcef55e65a2f8763d64e70bc5
martinezmatias <matias.sebastian.martinez@gmail.com>
2023-02-03 12:02:25 +0100

    update fix

--- tool_xTestCluster/runMissing.sh
+++ tool_xTestCluster/runMissing.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 for i in {303..320}
 do
- echo "$i"
- ./runGenAndExec.sh Math-59 $i;
- ./runGenAndExec.sh Lang-24 $i;
- ./runGenAndExec.sh Closure-31 $i;
- ./runGenAndExec.sh Math-71 $i;
- ./runGenAndExec.sh Chart-12 $i;
- ./runGenAndExec.sh Closure-46 $i;
- ./runGenAndExec.sh Chart-7 $i;
- ./runGenAndExec.sh Math-70 $i;
- ./runGenAndExec.sh Lang-10 $i;
- ./runGenAndExec.sh Lang-24 $i;
- ./runGenAndExec.sh Math-57 $i;
- ./runGenAndExec.sh Lang-39 $i;
- ./runGenAndExec.sh Lang-45 $i;
- ./runGenAndExec.sh Lang-55 $i;
- ./runGenAndExec.sh Math-4 $i;
- ./runGenAndExec.sh Lang-43 $i;
- ./runGenAndExec.sh Closure-14 $i;
- ./runGenAndExec.sh Closure-18 $i;
- ./runGenAndExec.sh Lang-6 $i;
- ./runGenAndExec.sh Math-33 $i;
- ./runGenAndExec.sh Math-58 $i;
- ./runGenAndExec.sh Closure-115 $i;
- ./runGenAndExec.sh Closure-62 $i;
- ./runGenAndExec.sh Closure-126 $i;
- ./runGenAndExec.sh Closure-38 $i;
+echo "$i"
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-59 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-24 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-31 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-71 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Chart-12 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-46 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Chart-7 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-70 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-10 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-24 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-57 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-39 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-45 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-55 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-4 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-43 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-14 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-18 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Lang-6 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-33 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Math-58 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-115 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-62 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-126 $i;
+/home/mmartinez/xTestCluster/tool_xTestCluster/runGenAndExec.sh Closure-38 $i;
 done
 echo "End"
