fa750ee003e9af794f05455a4d0dbd1009a50667
martinezmatias <matias.sebastian.martinez@gmail.com>
2023-02-17 17:35:45 +0100

    up ex jac

---
+++ tool_xTestCluster/runExecJacoco.sh
@@ -0,0 +1,6 @@
+cd /home/mmartinez/xTestCluster/tool_xTestCluster;
+python3 -m src.MainLauncher testexecution \
+/home/mmartinez/xTestCluster/Patches/Patches_Wang902/Patches_ICSE@/home/mmartinez/xTestCluster/Patches/Patches_Wang902/Patches_others \
+/home/mmartinez/xTestCluster/new_seed/newTestGenerated/seed_0Evosuite \
+/home/mmartinez/xTestCluster/new_seed/logsEvoJacoco/ \
+True;
